ce275dabde12455e5e6d6d622ca6102ab3bd0ebe
mtd1278 <maidinh1278@gmail.com>
2024-02-18 00:47:42 -0600

    changed to execvp

--- msh/msh.c
+++ msh/msh.c
@@ -55,7 +55,7 @@ void fork_and_exec_cmd(char * argv[])
   }
   else if (pid == 0)
   {
-    int i = execv(argv[0], argv);                                        
+    int i = execvp(argv[0], argv);                                       
     if( i == -1) 
     {
       char error_message[30] = "An error has occurred\n";              
@@ -136,6 +136,8 @@ void process_command_string(char * command_string)
       // if not exit or cd 
       if ((strcmp(token[0], "exit") != 0) && (strcmp(token[0], "cd") !=0 ) && token[0] != NULL)
       {
+        fork_and_exec_cmd(token); 
+        /*
         char path0[50] = "/bin/";
         char path1[50] = "/usr/bin/";
         char path2[50] = "/usr/local/bin/";
@@ -178,7 +180,7 @@ void process_command_string(char * command_string)
               }
             }
           }
-        }
+        }*/
       }
       else
       {
@@ -211,11 +213,11 @@ void process_command_string(char * command_string)
 
 int main( int argc, char * argv[] )
 {
-  char * command_string = (char*) malloc( MAX_COMMAND_SIZE ); // point to command string 
+  char * command_string = (char*) malloc( MAX_COMMAND_SIZE ); 
   
-  if (argc == 2)  // batch 
+  if (argc == 2)  
   {
-    FILE* file = fopen(argv[1], "r");   // bus error opening
+    FILE* file = fopen(argv[1], "r");   
     char *buffer= (char*)malloc(MAX_COMMAND_SIZE);
     if (file == NULL)
     {
@@ -230,7 +232,7 @@ int main( int argc, char * argv[] )
     }
   fclose(file);
   }
-  else if (argc == 1)   // interactive 
+  else if (argc == 1)   
   {                   
     while( 1 )
     {
@@ -243,134 +245,6 @@ int main( int argc, char * argv[] )
       // inputs something.
       while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
       process_command_string(command_string);
-
-      /* Parse input */
-      /*
-      char *token[MAX_NUM_ARGUMENTS];  
-
-      int token_count = 0;                                 
-                                                            
-      // Pointer to point to the token
-      // parsed by strsep
-      char *argument_pointer;                                         
-                                                            
-      char *working_string  = strdup( command_string );     // strdup reutns a pointer to a duplicated of command string, terminated by null     
-
-      // we are going to move the working_string pointer so
-      // keep track of its original value so we can deallocate
-      // the correct amount at the end
-      
-      char *head_ptr = working_string;  // head pointer to duplicatd string 
-      
-      // Tokenize the input with whitespace used as the delimiter
-      // argument pointer points to tokenized duplicated string 
-      // while token not null and less than max args
-      while ( ( (argument_pointer = strsep(&working_string, WHITESPACE ) ) != NULL) && (token_count<MAX_NUM_ARGUMENTS))                                       
-      {
-        token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );  // duplicate argument pointer to token 
-        if( strlen( token[token_count] ) == 0 )
-        {
-          token[token_count] = NULL;
-        }
-          token_count++;
-      }
-       // REDIRECTION
-  
-      int i;
-      for( i=1; i<token_count-1; i++ )
-      {
-        if( strcmp(token[i], ">") == 0 )
-        {
-            int fd = open( token[i+1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR ); // read/write, create file, usr read/writepermission bit 
-            if( fd < 0 )
-            {
-                perror( "Can't open output file." );
-                exit( 0 );                    
-            }
-            dup2( fd, 1 );
-            close( fd );
-            
-            // Trim off the > output part of the command
-            token[i] = NULL;
-            break;
-        }
-      } 
-      
-    
-  
-      // if not exit or cd 
-      if ((strcmp(token[0], "exit") != 0) && (strcmp(token[0], "cd") !=0 ) && token[0] != NULL)
-      {
-        char path0[50] = "/bin/";
-        char path1[50] = "/usr/bin/";
-        char path2[50] = "/usr/local/bin/";
-        char path3[50] = "./";
-        char *test[MAX_NUM_ARGUMENTS];
-        test[0]= strcat(path0, token[0]);
-        if (access(test[0], X_OK) == 0)
-        {
-          token[0] = test[0];
-          fork_and_exec_cmd(token); 
-        }
-        else
-        {
-          test[0] = strcat(path1, token[0]);
-          if (access(test[0], X_OK) == 0)
-          {
-            token[0] = test[0];
-            fork_and_exec_cmd(token); 
-          }
-          else
-          {
-            test[0] = strcat(path2, token[0]);
-            if (access(test[0], X_OK) == 0)
-            {
-              token[0] = test[0];
-              fork_and_exec_cmd(token); 
-            }
-            else
-            {
-              test[0] = strcat(path3, token[0]);
-              if (access(test[0], X_OK) == 0)
-              {
-                token[0] = test[0];
-                fork_and_exec_cmd(token); 
-              }
-              else
-              {
-                char error_message[50] = "An error has occurred\n";             
-                write(STDERR_FILENO, error_message, strlen(error_message));
-              }
-            }
-          }
-        }
-      }
-      else
-      {
-        if (token[0] != NULL)
-        {
-          if (strcmp(token[0], "exit") == 0 && token[1] == NULL)
-          {
-            exit(0);
-          }
-          else if (strcmp(token[0], "cd") == 0 && token[1] != NULL && token[2] == NULL)                         
-          {
-            if (chdir(token[1]) == -1)
-            {
-              char error_message[30] = "An error has occurred\n";              
-              write(STDERR_FILENO, error_message, strlen(error_message));
-              exit(1);
-            }
-          }
-          else
-          {
-            char error_message[30] = "An error has occurred\n";             
-            write(STDERR_FILENO, error_message, strlen(error_message));
-            exit(1);
-          }
-        }
-      }
-      free(head_ptr); */
     }
     free(command_string);
     return 0;
